35be6dfaa3e3bdc661dd4a0165f49d559333adb9
Franco Sylvestre <francosylprog@gmail.com>
2021-04-22 19:06:35 -0300

    Images loaded with Picasso and better desing item_product

--- app/src/main/java/com/example/mercadolibrechallenge/ui/viewholders/ProductsViewHolder.kt
+++ app/src/main/java/com/example/mercadolibrechallenge/ui/viewholders/ProductsViewHolder.kt
@@ -1,16 +1,28 @@
 package com.example.mercadolibrechallenge.ui.viewholders
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mercadolibrechallenge.R
 import com.example.mercadolibrechallenge.databinding.ListItemProductBinding
 import com.example.mercadolibrechallenge.network.responses.Product
+import com.squareup.picasso.Picasso
 
 class ProductsViewHolder(private var binding: ListItemProductBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind (product: Product, onProductClick: (product: Product) -> Unit){
         binding.apply {
+            val price = product.price?.toInt()
+            val context = this.root.context
+
             tvName.text = product.title
-            tvPrice.text = "$ ${product.price?.toInt()}"
-//            ivPhoto.setImageBitmap(convertImage(product.thumbnail))
+            tvPrice.text = context.getString(R.string.price, price.toString())
+            if (price ?: 0 > 3500){
+                tvFreeDelivery.visibility = View.VISIBLE
+            } else {
+                tvFreeDelivery.visibility = View.INVISIBLE
+            }
+
+            Picasso.with(context).load(product.thumbnail).fit().into(ivPhoto)
 
             containerProduct.setOnClickListener {
                 onProductClick(product)
